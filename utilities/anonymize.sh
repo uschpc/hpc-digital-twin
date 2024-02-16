e41ab6d9443b0dc0a785564f1b0d7235793d5c2e
Cesar Sul <csul@usc.edu>
2024-02-16 15:44:36 -0800

    only usernames required

--- utilities/anonymize.sh
+++ utilities/anonymize.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# sacct --starttime 10/01/23 --endtime 10/31/23 -a --format=User,JobID,Submit,Start,End,Timelimit%12,ReqCPUS,ReqNodes,Account%15,Partition,QOS,ReqMem,ReqTRES -X -P
+# sacct --starttime 10/01/23 --endtime 10/31/23 -a --format=User,JobID,Submit,Start,End,Timelimit%12,ReqCPUS,ReqNodes,Account%15,Partition,QOS,ReqMem,ReqTRES -X -P > slurm_sacct_data
 
-log_file="slurm_sacct_data_v2"
+log_file="slurm_sacct_data"
 anon_file="slurm_anon"
 
 echo "getting usernames"
 usernames=$(awk 'BEGIN {FS="|"} {print$1}' $log_file | sort | uniq)
-echo "getting account names"
-accounts==$(awk 'BEGIN {FS="|"} {print$9}' $log_file | sort | uniq)
 
 
 n_usernames=$(echo $usernames | wc -w)
-n_accounts=$(echo $accounts | wc -w)
 
 counter=1
 
@@ -22,14 +19,6 @@ for username in $usernames
 do
     echo "$username -> user$counter"
     sed_syntax="s/$username/user$counter/g"
-    sed -i $sed_syntax $anon_file
-    counter=$((counter+1))
-done
-counter=1
-for account in $accounts
-do
-    echo "$account -> account$counter"
-    sed_syntax="s/$account/account$counter/g"
     sed -i $sed_syntax $anon_file
     counter=$((counter+1))
 done
